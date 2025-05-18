49d56c5f3184d8148ae6f3678ca2f7423fe094cc
Алексей <101291963+Alexeykoh@users.noreply.github.com>
2025-05-18 12:42:50 +0300

    test: ci-cd

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import "./index.css";
 import App from "./App";
 import { ThemeProvider } from "./providers/theme-provider";
 
-// ci-cd test v2
+// ci-cd test v3
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
